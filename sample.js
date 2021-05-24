db85b069cfda3977f99cc7754baac20abcdeac69
Taisuke Fukuno <fukuno@jig.jp>
2021-05-25 08:18:19 +0900

    add sample

--- sample.js
+++ sample.js
@@ -1,8 +1,3 @@
-# PNG
-
-Pure JavaScript PNG encoder / decoder
-
-```js
 import { PNG } from "https://taisukef.github.io/PNG/PNG.js";
 
 const bin = PNG.encode(new Uint8ClampedArray([0xff, 0, 0, 0xff]), 1, 1);
@@ -10,7 +5,3 @@ console.log(bin, typeof bin);
 
 const img2 = PNG.decode(bin);
 console.log(img2);
-```
-
-
-
