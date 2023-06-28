0b0213722ccb79f8550b30aa2c52b3ace6b8eddf
Taisuke Fukuno <fukuno@jig.jp>
2023-06-29 07:42:02 +0900

    remove async version

--- sample_async.js
+++
@@ -1,7 +0,0 @@
-import { PNG } from "https://taisukef.github.io/PNG/PNG_async.js";
-
-const bin = await PNG.encode(new Uint8ClampedArray([0xff, 0, 0, 0xff]), 1, 1);
-console.log(bin, typeof bin);
-
-const img2 = await PNG.decode(bin);
-console.log(img2);
