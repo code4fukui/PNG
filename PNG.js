988193b83fe94ebced81a524b142b16e9e0665bb
Taisuke Fukuno <fukuno@jig.jp>
2024-05-14 08:02:09 +0900

    add decodeMulti

--- PNG.js
+++ PNG.js
@@ -1,9 +1,10 @@
 import { encodePNG } from "https://taisukef.github.io/PNGEncoder/encodePNG.js";
-import { decodePNG } from "https://taisukef.github.io/pngjs/decodePNG.js";
+import { decodePNG, decodePNGs } from "https://taisukef.github.io/pngjs/decodePNG.js";
 
 const PNG = {
   encode: (imgd) => encodePNG(imgd.data, imgd.width, imgd.height),
   decode: decodePNG,
+  decodeMulti: decodePNGs,
 };
 
 export { PNG };
