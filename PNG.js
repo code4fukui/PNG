e3f4fc6388caa784bf3a7196f2ac43d0ed8e0f8d
Taisuke Fukuno <fukuno@jig.jp>
2026-01-10 09:45:22 +0900

    add canDecode

--- PNG.js
+++ PNG.js
@@ -5,6 +5,15 @@ const PNG = {
   encode: (imgd) => encodePNG(imgd.data, imgd.width, imgd.height),
   decode: decodePNG,
   decodeMulti: decodePNGs,
+  canDecode: (bin) => {
+    if (!bin) return false;
+    const header = [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A];
+    if (bin.length < header.length) return false;
+    for (let i = 0; i < header.length; i++) {
+      if (bin[i] != header[i]) return false;
+    }
+    return true;
+  },
 };
 
 export { PNG };
--- test.js
+++ test.js
@@ -2,16 +2,21 @@ import * as t from "https://deno.land/std/testing/asserts.ts";
 import { PNG } from "./PNG.js";
 
 Deno.test("sync", () => {
-  const img = new Uint8Array(4);
+  const img = new Uint8ClampedArray(4);
   img[0] = 0xff;
   img[3] = 0xff;
-  console.log(img);
+  //console.log(img);
   const imgd = { data: img, width: 1, height: 1 };
   const bin = PNG.encode(imgd);
-  console.log(bin, typeof bin);
+  //console.log(bin, typeof bin);
   //await Deno.writeFile("test.png", bin);
   
   const img2 = PNG.decode(bin);
-  console.log(img2);
+  //console.log(img2);
   t.assertEquals(img2.data, img);
+});
+
+Deno.test("canDecode", async () => {
+  t.assertEquals(PNG.canDecode(await Deno.readFile("test.png")), true);
+  t.assertEquals(PNG.canDecode(await Deno.readFile("test.js")), false);
 });
